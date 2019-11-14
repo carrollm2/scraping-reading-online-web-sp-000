51ec14a8ee6c0368afe3880cf8f0e27ade299102
Michael Carroll <carrollm2@gmail.com>
2019-11-14 02:41:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,4 +5,4 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 #puts doc.css(".headline-26OIBN").text.strip
-puts doc.css(".content-30TK_R")
+puts doc.css(".content-3OTK_R")
